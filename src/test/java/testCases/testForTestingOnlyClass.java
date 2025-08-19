8c9a2042b71c98d0d5d979450c06063e69333ff1
Kent <kent.gigante@gmail.com>
2025-08-19 08:59:52 +0800

    Updates config file for local execution

--- src/test/java/testCases/testForTestingOnlyClass.java
+++
@@ -1,9 +0,0 @@
-package testCases;
-
-public class testForTestingOnlyClass {
-	public static void main(String[] testOnly)
-	{
-		String getUser = System.getProperty("user.name");
-		System.out.println("user.name returns: " + getUser);
-	}
-}
